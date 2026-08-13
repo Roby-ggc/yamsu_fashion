accddeb0f8b9fbd88262cb503f02f562ef86aff7
Hinimzina Robert Madjougoura <gayacamlot@gmail.com>
2026-08-13 13:04:29 +0100

    fix cloudinary images

--- yamsu_fashion/settings.py
+++ yamsu_fashion/settings.py
@@ -189,17 +189,12 @@ CLOUDINARY_STORAGE = {
 STORAGES = {
 
     "default": {
-
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-
     },
 
-
     "staticfiles": {
-
         "BACKEND":
         "whitenoise.storage.CompressedManifestStaticFilesStorage",
-
     },
 
 }
